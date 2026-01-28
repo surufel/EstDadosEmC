1990063a18a2bb0815d023280dd79aad53074d37
surufel <henriquefrancasmm@gmail.com>
2026-01-28 18:26:15 -0300

    feat: método push para pilha

--- Pilha/pilha.c
+++ Pilha/pilha.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define TAMANHO 20
+#define TAMANHO 20 // de 0 a 19
 
 typedef struct pilha{
 	int pilha[TAMANHO];
 	int topo;
 }Pilha;
+
+void push(int valor, Pilha *x){
+	if(x->topo < TAMANHO){
+	x->pilha[x->topo] = valor;
+	x->topo++;
+	} else {
+	printf("Pilha estourou, ela esta cheia");
+	}
+}
+
 
 int main(){
 	Pilha p;
@@ -15,7 +25,6 @@ int main(){
 	p.pilha[p.topo] = 100; // Empilhando o numero 100
 	p.topo++; // Total na pilha: 1
 	p.topo--; // Total na pilha: 0
-	
 	return 0;
 }
 
