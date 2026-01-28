3fc6550001b747a9c393c0a26f23cde335a05b11
surufel <henriquefrancasmm@gmail.com>
2026-01-28 18:08:06 -0300

    Empilhando e desempilhando a pilha

--- Pilha/pilha.c
+++ Pilha/pilha.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define TAMANHO 20
+
 typedef struct pilha{
 	int pilha[TAMANHO];
 	int topo;
 }Pilha;
 
 int main(){
+	Pilha p;
+	p.topo = 0;
 
+	p.pilha[p.topo] = 100; // Empilhando o numero 100
+	p.topo++; // Total na pilha: 1
+	p.topo--; // Total na pilha: 0
+	
+	return 0;
 }
 
